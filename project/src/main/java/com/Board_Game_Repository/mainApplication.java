9cac62b0fb98d6b4aeac9442a50b391df43b1e7f
Ionita Alexandru-Mihail <aionita1001@stud.fils.upb.ro>
2024-05-16 11:33:36 +0300

    Created the Java Spring project, with its dependencies, arranged the file structure for the Spring project; added .gitignore

---
+++ project/src/main/java/com/Board_Game_Repository/mainApplication.java
@@ -0,0 +1,13 @@
+package com.Board_Game_Repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameRepositoryApplication.class, args);
+	}
+
+}
---
+++ project/src/test/java/com/Board_Game_Repository/BoardGameRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Board_Game_Repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
